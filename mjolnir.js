4ba6750b0a0b664f4677624e2dceff79dd8d2388
3rd-Eden <info@3rd-Eden.com>
2013-04-09 17:12:31 +0200

    [minor] Added better progress reporting by writing on a interval. [fix] Started the last message AFTER we have received the data.

--- mjolnir.js
+++ mjolnir.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var Socket = require('ws')
-  , connections = {};
+  , connections = {}
+  , concurrent = 0;
 
 //
 // Get the session document that is used to generate the data.
@@ -44,7 +45,7 @@ process.on('message', function message(task) {
   });
 
   socket.on('open', function open() {
-    process.send({ type: 'open', duration: Date.now() - now, id: task.id });
+    process.send({ type: 'open', duration: Date.now() - now, id: task.id, concurrent: concurrent });
     write(socket, task, task.id);
 
     // As the `close` event is fired after the internal `_socket` is cleaned up
@@ -53,7 +54,7 @@ process.on('message', function message(task) {
 
   socket.on('message', function message(data) {
     process.send({
-      type: 'message', latency: Date.now() - socket.last,
+      type: 'message', latency: Date.now() - socket.last, concurrent: concurrent,
       id: task.id
     });
 
@@ -69,20 +70,23 @@ process.on('message', function message(task) {
     var internal = socket._socket || {};
 
     process.send({
-      type: 'close', id: task.id,
+      type: 'close', id: task.id, concurrent: --concurrent,
       read: internal.bytesRead || 0,
       send: internal.bytesWritten || 0
     });
+
+    delete connections[task.id];
   });
 
   socket.on('error', function error(err) {
-    process.send({ type: 'error', message: err.message, id: task.id });
+    process.send({ type: 'error', message: err.message, id: task.id, concurrent: --concurrent });
 
     socket.close();
     delete connections[task.id];
   });
 
   // Adding a new socket to our socket collection.
+  ++concurrent;
   connections[task.id] = socket;
 });
 
@@ -96,15 +100,15 @@ process.on('message', function message(task) {
  * @api private
  */
 function write(socket, task, id, fn) {
-  var start = socket.last = Date.now();
-
   session[binary ? 'binary' : 'utf8'](task.size, function message(err, data) {
+    var start = socket.last = Date.now();
+
     socket.send(data, {
       binary: binary,
       mask: masked
     }, function sending(err) {
       if (err) {
-        process.send({ type: 'error', message: err.message });
+        process.send({ type: 'error', message: err.message, concurrent: --concurrent, id: id });
 
         socket.close();
         delete connections[id];
