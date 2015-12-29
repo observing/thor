9545ce29efc24eb58b7e17af027a4d18c072f604
unknown <iorichina@gmail.com>
2015-12-29 18:09:48 +0800

    add log error, fix concurrent summary

--- worker-socketio.js
+++ worker-socketio.js
@@ -19,6 +19,7 @@ var masked = process.argv[4] === 'true'
 // collect metics datas
 var metrics_datas = {collection:true, datas:[]}
   , statInterval = +process.argv[6] || 60
+  , logError = +process.argv[7] || 0
   , process_send = function(data, task) {
       if (statInterval <= 0 || ('open' == data.type && task.nextTask)) {
         process.send(data);
@@ -51,7 +52,7 @@ var metrics_datas = {collection:true, datas:[]}
     }, statInterval * 1000);
 
 process.on('message', function message(task) {
-  var now = Date.now();
+  var start_timestamp = Date.now();
 
   //
   // Write a new message to the socket. The message should have a size of x
@@ -77,41 +78,27 @@ process.on('message', function message(task) {
   var socket = new Socket(task.url, {
     'force new connection': true,
     reconnection: false,
-    timeout: 5000,
-    transports: [task.transport],
+    timeout: task.connectTimeout * 1000,
+    transports: task.transport,
     protocolVersion: protocol,
-    localAddress: task.localaddr || null
+    localAddress: task.localaddr || null,
+    headers: {'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.93 Safari/537.36'}
   });
   socket.last = Date.now();
   var pingInterval = null;
 
   socket.on('connect', function open() {
-    process_send({ type: 'open', duration: Date.now() - now, id: task.id, concurrent: concurrent, workerid: process.pid }, task);
+    process_send({ type: 'open', duration: Date.now() - start_timestamp, id: task.id, concurrent: concurrent, cur_time: Date.now(), wid: process.pid }, task);
     // write(socket, task, task.id);
 
     // As the `close` event is fired after the internal `_socket` is cleaned up
     // we need to do some hacky shit in order to tack the bytes send.
   });
 
-  socket.on('message', function message(data) {
+  socket.on(process.env.NODE_ON_MESSAGE?process.env.NODE_ON_MESSAGE:'message', function message(data) {
     process_send({
-      type: 'message', latency: Date.now() - socket.last, concurrent: concurrent, workerid: process.pid,
-      id: task.id
-    }, task);
-
-    // Only write as long as we are allowed to send messages
-    if (task.messages > 0)
-    if (--task.messages) {
-      write(socket, task, task.id);
-    } else {
-      socket.disconnect();
-      socket.emit('disconnect');
-    }
-  });
-  socket.on('onMessage', function onMessage(data) {
-    process_send({
-      type: 'message', latency: Date.now() - socket.last, concurrent: concurrent, workerid: process.pid,
-      id: task.id
+      type: 'message', latency: Date.now() - socket.last,
+      id: task.id, wid: process.pid
     }, task);
 
     // Only write as long as we are allowed to send messages
@@ -124,7 +111,12 @@ process.on('message', function message(task) {
     }
   });
 
-  socket.on('disconnect', function close() {
+  socket.on('disconnect', function close(msg) {
+    var err = msg=='error' && arguments.length > 2 ? Array.prototype.slice.call(arguments, 1, 2).pop() : null;
+    if (err && logError) {
+      console.error(err);
+    }
+
     var internal = {};
     try{
       internal = socket.io.engine.transport.ws._socket || {};
@@ -133,7 +125,7 @@ process.on('message', function message(task) {
     }
 
     process_send({
-      type: 'close', id: task.id, concurrent: --concurrent, workerid: process.pid,
+      type: 'close', id: task.id, wid: process.pid, concurrent: --concurrent, cur_time: Date.now(),
       read: internal.bytesRead || 0,
       send: internal.bytesWritten || 0
     }, task);
@@ -144,20 +136,20 @@ process.on('message', function message(task) {
   });
 
   socket.on('error', function error(err) {
-    process_send({ type: 'error', message: err.description ? err.description.message : err.message, id: task.id, concurrent: --concurrent, workerid: process.pid }, task);
+    process_send({ type: 'error', message: err.description ? err.description.message : (err.message?err.message:err), id: task.id, wid: process.pid }, task);
 
-    socket.disconnect();
-    socket.emit('disconnect');
+    socket.disconnect('error', err);
+    socket.emit('disconnect', 'error', err);
     delete connections[task.id];
     checkConnectionLength();
   });
 
   // catch ECONNREFUSED
-  socket.io.on('connect_error', function(err){
-    process_send({ type: 'error', message: err.description ? err.description.message : err.message, id: task.id, concurrent: --concurrent, workerid: process.pid }, task);
+  socket.io.on('connect_error', function connect_error(err){
+    process_send({ type: 'error', message: err.description ? err.description.message : (err.message?err.message:err), id: task.id, wid: process.pid }, task);
 
-    socket.disconnect();
-    socket.emit('disconnect');
+    socket.disconnect('error', err);
+    socket.emit('disconnect', 'error', err);
     delete connections[task.id];
     checkConnectionLength();
   });
@@ -198,10 +190,10 @@ function write(socket, task, id, fn, data) {
       mask: masked
     }, function sending(err) {
       if (err) {
-        process_send({ type: 'error', message: err.message, concurrent: --concurrent, workerid: process.pid, id: id }, task);
+        process_send({ type: 'error', message: err.description ? err.description.message : (err.message?err.message:err), id: task.id, wid: process.pid }, task);
 
-        socket.disconnect();
-        socket.emit('disconnect');
+        socket.disconnect('error', err);
+        socket.emit('disconnect', 'error', err);
         delete connections[id];
       }
 
